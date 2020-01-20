bd4fc0a5aac6e0ad34e2a8e69b3311eb72ed4ad9
Dan Stroot <dan.stroot@gmail.com>
2020-01-19 17:00:29 -0800

    dependency updates

--- src/components/BackgroundWash/index.test.js
+++ src/components/BackgroundWash/index.test.js
@@ -3,20 +3,21 @@
 // object that we can extract asFragment from. Using this we can use the
 // built-in functionality from Jest to perform a snapshot test.
 
-import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
 
 // component to test
-import BackgroundWash from '.';
+import { BackgroundWash } from ".";
 
 // should be in setupTests.js?
 afterEach(cleanup);
 
-it('renders', () => {
+it("renders", () => {
   const { asFragment } = render(
     <BackgroundWash
-      filterOpts={['to bottom right', 'teal', 'blue', 'purple']}
+      filterOpts={["to bottom right", "teal", "blue", "purple"]}
     />
   );
   expect(asFragment()).toMatchSnapshot();
 });
+
